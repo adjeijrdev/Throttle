f39ec9c582680d67ea9df76a61b528fe34ea8651
Adjei Daniel Junior <adjeijuniordaniel@gmail.com>
2025-04-14 16:38:59 +0000

    registration page was setup

--- src/Components/SideLink.jsx
+++ src/Components/SideLink.jsx
@@ -1,5 +1,5 @@
 // import { useState } from "react";
-// import { useNavigate } from "react-router";
+import { useNavigate } from "react-router";
 
 import { NavLink } from "react-router";
 import "./SideLink.css";
@@ -8,10 +8,11 @@ import "./SideLink.css";
 // import DashBoardLogo from "../Assets/icons/dashboard.png";
 
 export default function ({ icon, name, url }) {
+  const navigate = useNavigate();
   return (
-    <div className="Link">
-      <img src={icon} alt={name} />
-      <NavLink to={url}> {name} </NavLink>
+    <div className="Link" onClick={() => nnnavigate(url) } >
+      <img src={icon} alt={name}/>
+      <NavLink to={url}>{name} </NavLink>
     </div>
   );
 }
