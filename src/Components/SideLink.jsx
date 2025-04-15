e0034741570550a75e1a236577ebd86e4cf10ac7
Adjei Daniel Junior <adjeijuniordaniel@gmail.com>
2025-04-15 14:45:51 +0000

    tabs active style added with logout button

--- src/Components/SideLink.jsx
+++ src/Components/SideLink.jsx
@@ -1,18 +1,27 @@
-// import { useState } from "react";
-import { useNavigate } from "react-router";
+import { useState } from 'react';
+import { useNavigate,NavLink  } from "react-router";
 
-import { NavLink } from "react-router";
 import "./SideLink.css";
 
 //import components
 // import DashBoardLogo from "../Assets/icons/dashboard.png";
 
-export default function ({ icon, name, url }) {
+
+
+export default function ({ icon, name, url, isActive, setActive }) {
   const navigate = useNavigate();
+  
+  function clickedTab(){
+      navigate(url)
+      setActive(url)
+
+  }
   return (
-    <div className="Link" onClick={() => nnnavigate(url) } >
-      <img src={icon} alt={name}/>
-      <NavLink to={url}>{name} </NavLink>
+    <div className={isActive?"Link-active":"Link"} onClick={clickedTab}>
+      <div className={isActive?"active-logo" : ""} >
+        <img src={icon} alt={name}/>
+      </div>
+      <NavLink to={url}> {name} </NavLink>
     </div>
   );
 }
