78034438ad4a76022e4c572785f9ee8b3f01cb80
Adjei Daniel Junior <adjeijuniordaniel@gmail.com>
2025-05-05 04:07:31 +0000

    add the esc button to delete and animations

--- src/Modals/ModalComponents/SignupCard.jsx
+++ src/Modals/ModalComponents/SignupCard.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router";
 import "./SignupCard.css";
 
@@ -9,14 +9,25 @@ import Rider from "../../Assets/icons/Rider.png";
 export default function SignupCard({ onClose }) {
   const navigate = useNavigate();
 
+  useEffect(() => {
+    const handleEsc = (e) => {
+      if (e.key === "Escape") onClose();
+    };
+    window.addEventListener("keydown", handleEsc);
+    return () => window.removeEventListener("keydown", handleEsc);
+  }, [onClose]);
+
   const handleCardClick = (e) => e.stopPropagation();
 
   return (
-    <div className="modal-background" onClick={onClose }>
-      <div className="register-modal-card"  onClick={handleCardClick}>
+    <div className="modal-background" onClick={onClose}>
+      <div className="register-modal-card" onClick={handleCardClick}>
         <h2>Register as?</h2>
         <img src={Vendors} alt="v" />
-        <div className="register-modal-button" onClick={() => navigate("/register")}>
+        <div
+          className="register-modal-button"
+          onClick={() => navigate("/register")}
+        >
           <img src={Vendors} alt="vendors-logo" />
           <p>Vendor</p>
         </div>
