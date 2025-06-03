f998da9648ee16ac5c8d51cb3ca0e38850309f90
Adjei Daniel Junior <adjeijuniordaniel@gmail.com>
2025-06-03 02:38:07 +0000

    register form editted

--- src/Components/RiderStepper.jsx
+++ src/Components/RiderStepper.jsx
@@ -1,6 +1,6 @@
 import { useState, useRef } from "react";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
-import style from "./Stepper.module.css";
+import style from "./RiderStepper.module.css";
 
 //import images
 import LeftSVG from "../Assets/icons/Left.png";
@@ -17,7 +17,7 @@ const steps = [
   "Vehicle Details",
   "Payment & Billing",
   "Document Uploads",
-  "Account Details"
+  "Account Details",
 ];
 
 const Stepper = ({ name }) => {
@@ -142,10 +142,7 @@ const Stepper = ({ name }) => {
 
   return (
     <form onSubmit={handleSubmit} className={style["form-container"]}>
-      <h2 className={style["form-title"]}>
-        {" "}
-        Registration process as a {name}{" "}
-      </h2>
+      <h2 className={style["form-title"]}>Registration process as a {name}</h2>
 
       <div className={style["stepper"]}>
         {steps.map((label, index) => (
@@ -165,255 +162,260 @@ const Stepper = ({ name }) => {
           </div>
         ))}
       </div>
-
-      <div className={style["form-step"]}>
-        {currentStep === 0 && (
-          <div className={style["form-grid"]}>
-            <div className={style["form-group"]}>
-              <label>Business Name</label>
-              <input
-                type="text"
-                name="businessname"
-                value={formData.businessname}
-                onChange={handleChange}
-                placeholder="business name"
-              />
-            </div>
-            <div className={style["form-group"]}>
-              <label>Business Address</label>
-              <input
-                type="text"
-                name="businessaddress"
-                value={formData.businessaddress}
-                onChange={handleChange}
-                placeholder="address"
-              />
-            </div>
-            <div className={style["form-group"]}>
-              <label>Business Type (e.g. Electronics, Clothing)</label>
-              <input
-                type="text"
-                name="businesstype"
-                value={formData.businesstype}
-                onChange={handleChange}
-                placeholder="business type"
-              />
-            </div>
-            <div className={style["form-group"]}>
-              <label>Country & City of Operation</label>
-              <input
-                type="text"
-                name="country"
-                value={formData.country}
-                onChange={handleChange}
-                placeholder="country"
-              />
-            </div>
-            <div className={style["form-group"]}>
-              <label>Business Registration Number</label>
-              <input
-                type="text"
-                name="regnumber"
-                value={formData.regnumber}
-                onChange={handleChange}
-                placeholder="number"
-              />
-            </div>
-            <div className={style["form-group"]}>
-              <label>Years in Operation</label>
-              <input
-                type="text"
-                name="years"
-                value={formData.years}
-                onChange={handleChange}
-                placeholder="year"
-              />
-            </div>
-          </div>
-        )}
-        {/* step 2- Contact Details */}
-        {currentStep === 1 && (
-          <div className={style["form-grid"]}>
-            <div className={style["form-group"]}>
-              <label> Name</label>
-              <input
-                type="text"
-                name="vendorname"
-                value={formData.vendorname}
-                onChange={handleChange}
-                placeholder="name"
-              />
-            </div>
-            <div className={style["form-group"]}>
-              <label>Email</label>
-              <input
-                type="text"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                placeholder="email"
-              />
-            </div>
-            <div className={style["form-group"]}>
-              <label>Phone Number</label>
-              <input
-                type="text"
-                name="phone"
-                value={formData.phone}
-                onChange={handleChange}
-                placeholder="number"
-              />
-            </div>
-            <div className={style["form-group"]}>
-              <label>Business Website</label>
-              <input
-                type="text"
-                name="website"
-                value={formData.website}
-                onChange={handleChange}
-                placeholder="link"
-              />
-            </div>
-          </div>
-        )}
-
-        {/* Step 3 - Payment & Billing */}
-        {currentStep === 2 && (
-          <div className={style["form-grid"]}>
-            <div className={style["form-group"]}>
-              <label>Bank Name</label>
-              <input
-                type="text"
-                name="bankname"
-                value={formData.bankname}
-                onChange={handleChange}
-                placeholder="bank name"
-              />
-            </div>
-            <div className={style["form-group"]}>
-              <label>Mobile Money Name</label>
-              <input
-                type="text"
-                name="momoname"
-                value={formData.momoname}
-                onChange={handleChange}
-                placeholder="momo name"
-              />
-            </div>
-            <div className={style["form-group"]}>
-              <label>Bank Account Number</label>
-              <input
-                type="text"
-                name="banknumber"
-                value={formData.banknumber}
-                onChange={handleChange}
-                placeholder=" account number"
-              />
-            </div>
-            <div className={style["form-group"]}>
-              <label>Mobile Money Number</label>
-              <input
-                type="text"
-                name="momonumber"
-                value={formData.momonumber}
-                onChange={handleChange}
-                placeholder="momo number"
-              />
-            </div>
-          </div>
-        )}
-
-        {/* Step 4 - Document Uploads */}
-        {currentStep === 3 && (
-          <div className={style["upload-container"]}>
-            <p>Business logo</p>
-            <div
-              className={style["drop-box"]}
-              onDrop={handleDrop}
-              onDragOver={(e) => e.preventDefault()}
-              onDragEnter={(e) => e.preventDefault()}
-            >
-              {image ? (
-                <img
-                  src={image.url}
-                  alt="Preview"
-                  className={style["preview-image"]}
+      <div className={style["form-input-container"]}>
+        <div className={style["form-step"]}>
+          {currentStep === 0 && (
+            <div className={style["form-grid"]}>
+              <div className={style["form-group"]}>
+                <label>Business Name</label>
+                <input
+                  type="text"
+                  name="businessname"
+                  value={formData.businessname}
+                  onChange={handleChange}
+                  placeholder="business name"
                 />
-              ) : (
-                <img src={img} alt="image-vector" />
-              )}
+              </div>
+              <div className={style["form-group"]}>
+                <label>Business Address</label>
+                <input
+                  type="text"
+                  name="businessaddress"
+                  value={formData.businessaddress}
+                  onChange={handleChange}
+                  placeholder="address"
+                />
+              </div>
+              <div className={style["form-group"]}>
+                <label>Business Type (e.g. Electronics, Clothing)</label>
+                <input
+                  type="text"
+                  name="businesstype"
+                  value={formData.businesstype}
+                  onChange={handleChange}
+                  placeholder="business type"
+                />
+              </div>
+              <div className={style["form-group"]}>
+                <label>Country & City of Operation</label>
+                <input
+                  type="text"
+                  name="country"
+                  value={formData.country}
+                  onChange={handleChange}
+                  placeholder="country"
+                />
+              </div>
+              <div className={style["form-group"]}>
+                <label>Business Registration Number</label>
+                <input
+                  type="text"
+                  name="regnumber"
+                  value={formData.regnumber}
+                  onChange={handleChange}
+                  placeholder="number"
+                />
+              </div>
+              <div className={style["form-group"]}>
+                <label>Years in Operation</label>
+                <input
+                  type="text"
+                  name="years"
+                  value={formData.years}
+                  onChange={handleChange}
+                  placeholder="year"
+                />
+              </div>
             </div>
-
-            <button className={style["upload-button"]} onClick={openFileDialog}>
-              Upload
-            </button>
-
-            <input
-              type="file"
-              accept="image/*"
-              ref={fileInputRef}
-              style={{ display: "none" }}
-              onChange={handleFileInputChange}
-            />
-          </div>
-        )}
-
-        {/* Step 5 - Account Details */}
-        {currentStep === 4 && (
-          <div className={style["form__grid"]}>
-             <label>Account Email</label>
-            <div className={`${style["Account-details-password"]} ${style.email}`}>
-              <img src={EmailIcon} alt="emailIcon" />
-              <input
-                type="text"
-                name="accountemail"
-                value={formData.accountemail}
-                onChange={handleChange}
-                placeholder="Enter your email"
-              />
+          )}
+          {/* step 2- Contact Details */}
+          {currentStep === 1 && (
+            <div className={style["form-grid"]}>
+              <div className={style["form-group"]}>
+                <label> Name</label>
+                <input
+                  type="text"
+                  name="vendorname"
+                  value={formData.vendorname}
+                  onChange={handleChange}
+                  placeholder="name"
+                />
+              </div>
+              <div className={style["form-group"]}>
+                <label>Email</label>
+                <input
+                  type="text"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  placeholder="email"
+                />
+              </div>
+              <div className={style["form-group"]}>
+                <label>Phone Number</label>
+                <input
+                  type="text"
+                  name="phone"
+                  value={formData.phone}
+                  onChange={handleChange}
+                  placeholder="number"
+                />
+              </div>
+              <div className={style["form-group"]}>
+                <label>Business Website</label>
+                <input
+                  type="text"
+                  name="website"
+                  value={formData.website}
+                  onChange={handleChange}
+                  placeholder="link"
+                />
+              </div>
             </div>
-             <label>Password</label>
-            <div className={style["Account-details-password"]}>
-              <img src={padLock} alt="padlock" />
-              <input
-                type={showPassword ? "text" : "password"}
-                name="password"
-                value={formData.password}
-                onChange={handleChange}
-                placeholder="password"
-              />
-              <button
-                type="button"
-                onClick={toggleVisibility}
-                className={style["toggle - btn"]}
+          )}
+
+          {/* Step 3 - Payment & Billing */}
+          {currentStep === 2 && (
+            <div className={style["form-grid"]}>
+              <div className={style["form-group"]}>
+                <label>Bank Name</label>
+                <input
+                  type="text"
+                  name="bankname"
+                  value={formData.bankname}
+                  onChange={handleChange}
+                  placeholder="bank name"
+                />
+              </div>
+              <div className={style["form-group"]}>
+                <label>Mobile Money Name</label>
+                <input
+                  type="text"
+                  name="momoname"
+                  value={formData.momoname}
+                  onChange={handleChange}
+                  placeholder="momo name"
+                />
+              </div>
+              <div className={style["form-group"]}>
+                <label>Bank Account Number</label>
+                <input
+                  type="text"
+                  name="banknumber"
+                  value={formData.banknumber}
+                  onChange={handleChange}
+                  placeholder=" account number"
+                />
+              </div>
+              <div className={style["form-group"]}>
+                <label>Mobile Money Number</label>
+                <input
+                  type="text"
+                  name="momonumber"
+                  value={formData.momonumber}
+                  onChange={handleChange}
+                  placeholder="momo number"
+                />
+              </div>
+            </div>
+          )}
+
+          {/* Step 4 - Document Uploads */}
+          {currentStep === 3 && (
+            <div className={style["upload-container"]}>
+              <p>Business logo</p>
+              <div
+                className={style["drop-box"]}
+                onDrop={handleDrop}
+                onDragOver={(e) => e.preventDefault()}
+                onDragEnter={(e) => e.preventDefault()}
               >
-                {showPassword ? <FaEyeSlash /> : <FaEye />}
-              </button>
-            </div>
-            <label>Confirm Password</label>
-            <div className={style["Account-details-password"]}>
-              <img src={padLock} alt="padlock" />
-              <input
-                type={showPassword1 ? "text" : "password" }
-                name="confirmpassword"
-                value={formData.confirmpassword}
-                onChange={handleChange}
-                placeholder="Confirm Password"
-              />
-              <button
-                type="button"
-                onClick={toggleVisibility1}
-                className={style["toggle - btn"]}
-              >
-                { showPassword1 ? <FaEyeSlash /> : <FaEye />}
-              </button>
-            </div>
-          </div>
-        )}
+                {image ? (
+                  <img
+                    src={image.url}
+                    alt="Preview"
+                    className={style["preview-image"]}
+                  />
+                ) : (
+                  <img src={img} alt="image-vector" />
+                )}
+              </div>
 
-        {/* You can duplicate the logic above for step 1, 2, 3, 4 content */}
+              <button
+                className={style["upload-button"]}
+                onClick={openFileDialog}
+              >
+                Upload
+              </button>
+
+              <input
+                type="file"
+                accept="image/*"
+                ref={fileInputRef}
+                style={{ display: "none" }}
+                onChange={handleFileInputChange}
+              />
+            </div>
+          )}
+
+          {/* Step 5 - Account Details */}
+          {currentStep === 4 && (
+            <div className={style["form__grid"]}>
+              <label>Account Email</label>
+              <div
+                className={`${style["Account-details-password"]} ${style.email}`}
+              >
+                <img src={EmailIcon} alt="emailIcon" />
+                <input
+                  type="text"
+                  name="accountemail"
+                  value={formData.accountemail}
+                  onChange={handleChange}
+                  placeholder="Enter your email"
+                />
+              </div>
+              <label>Password</label>
+              <div className={style["Account-details-password"]}>
+                <img src={padLock} alt="padlock" />
+                <input
+                  type={showPassword ? "text" : "password"}
+                  name="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                  placeholder="password"
+                />
+                <button
+                  type="button"
+                  onClick={toggleVisibility}
+                  className={style["toggle - btn"]}
+                >
+                  {showPassword ? <FaEyeSlash /> : <FaEye />}
+                </button>
+              </div>
+              <label>Confirm Password</label>
+              <div className={style["Account-details-password"]}>
+                <img src={padLock} alt="padlock" />
+                <input
+                  type={showPassword1 ? "text" : "password"}
+                  name="confirmpassword"
+                  value={formData.confirmpassword}
+                  onChange={handleChange}
+                  placeholder="Confirm Password"
+                />
+                <button
+                  type="button"
+                  onClick={toggleVisibility1}
+                  className={style["toggle - btn"]}
+                >
+                  {showPassword1 ? <FaEyeSlash /> : <FaEye />}
+                </button>
+              </div>
+            </div>
+          )}
+
+          {/* You can duplicate the logic above for step 1, 2, 3, 4 content */}
+        </div>
       </div>
-
       <div className={style.buttons}>
         {currentStep > 0 && (
           <div className={style["btn-outline"]} onClick={prevStep}>
