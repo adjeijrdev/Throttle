25bd20fc290e4b01cbe853f5647a345465c8179f
MotteyAmos <motteyamos770@gmail.com>
2025-07-05 00:28:57 +0000

    worked on sidebar

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -7,6 +7,7 @@ import Logout from "../Assets/icons/logout.png";
 import SideLink from "../Components/SideLink";
 import { SideNavLinks } from "../items/Links";
 import dp from "../Assets/Models/dp.webp";
+import { LuPanelLeftClose, LuPanelRightClose } from "react-icons/lu";
 
 //LINKS
 import DashBoardLogo from "../Assets/icons/dashboard.png";
@@ -14,15 +15,20 @@ import DashBoardLogo from "../Assets/icons/dashboard.png";
 export default function HomeLayout() {
   const navigate = useNavigate();
   const [activeTab, setActiveTab] = useState("/");
+  const [closeSideBar, setCloseSideBar] = useState(false);
   // console.log(activeTab)
 
   return (
-    <div className="main-container">
-      <div className="left-side-bar">
-        <div className="logo" onClick={() => navigate("/login")}>
-          <img src={Logo} alt="Logo" />
+    <div className= {`${!closeSideBar ? "main-container" : " main-container-close-mode"}`} >
+      <div className= {` ${!closeSideBar ? " left-side-bar " : "left-side-bar-close"} close-st-bar`}>
+        <div className="logo">
+         <img src={Logo} alt="Logo" /> 
+          {
+          
+            !closeSideBar && <LuPanelLeftClose className="close-bar-st" onClick={()=>setCloseSideBar(true)}/>
+          }
         </div>
-        <div className="link_container">
+        <div className= "link_container">
           <div className="links">
           {SideNavLinks.map((Nav) => (
             <SideLink
@@ -35,19 +41,20 @@ export default function HomeLayout() {
               isActive={activeTab === Nav.url}
             />
           ))}
+        </div>  
         </div>
         < div className="logout-botton" onClick={() => navigate("/login")}>
             <img src={Logout} alt="logout" />
             <Link to="/login"> Logout</Link>
         </div>
-      
-          
-        </div>
       </div>
 
       <div className="right-section">
         <div className="header">
+          <LuPanelRightClose onClick={()=>setCloseSideBar(false)} className={`${closeSideBar? "close-bar-st" :"close-bar-st-hide" }`}   />
+        
           <div className="user-card">
+            
             <div className="text">
               <h4>John Doe </h4>
               <p>Manager</p>
